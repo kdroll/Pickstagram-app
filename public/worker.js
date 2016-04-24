8aedcdbb490ad92d69fc8a85b750944f4a48983a
Keith Droll <kdroll@purdue.edu>
2016-04-24 05:16:59 +0000

    added shit

---
+++ public/worker.js
@@ -0,0 +1,15 @@
+/*eslint-env browser */
+function goToRegisterPage() {
+	//console.log("TEST~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+
+	//var submittedName = document.getElementById('name').value;
+	//alert("Hello, " + submittedName + "!");
+	
+	window.location = 'register.html';
+}
+
+function login() {
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	window.location = 'main.html';
+}
