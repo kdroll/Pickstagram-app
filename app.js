a5938c7bea5d03093389377ebc3bb77ac4011811
Nicolas Bratton <nbratton@purdue.edu>
2016-04-25 04:42:32 +0000

    Able to add "require('mysql') to app.js. Unfortunately the site crashes after like 20 seconds because of some sort of an unhandled error that I can't find. Going to sleep.

--- app.js
+++ app.js
@@ -4,9 +4,27 @@
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
 
+// var connection = mysql.createConnection({});
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+console.log("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~test");
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+ 	host : 'us-cdbr-iron-east-03.cleardb.net',
+	user : 'b2ec527dbce1db',
+	password : '48ec4705'
+});
+//connection.connect(function() {
+  // connected! (unless `err` is set)
+//});
+var post  = {id: 1, title: 'Hello MySQL'};
+var query = connection.query('INSERT INTO posts SET ?', post, function(err, result) {
+  // Neat!
+});
+console.log(query.sql); // INSERT INTO posts SET `id` = 1, `title` = 'Hello MySQL'
+console.log(connection);
+console.log("test~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
--- public/worker.js
+++ public/worker.js
@@ -1,4 +1,6 @@
 /*eslint-env browser */
+var accountsArray = [];
+
 function goToRegisterPage() {
 	//console.log("TEST~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
 
@@ -11,10 +13,22 @@ function goToRegisterPage() {
 function login() {
 	var username = document.getElementById('username').value;
 	var password = document.getElementById('password').value;
-
+	
 	if (username == "" || password == ""){
 		alert("Please enter a username and password");
 	} else {
 		window.location = 'main.html';
 	}
+}
+
+function register() {
+	var username = document.getElementById('username').value;
+	var password = document.getElementById('password').value;
+	
+	var concat = username + "/" + password;
+	accountsArray[accountsArray.length] = concat;
+	
+	alert(accountsArray[0]);
+	
+	window.location = 'main.html';
 }
